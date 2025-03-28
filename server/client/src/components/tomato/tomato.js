784f35f0facaa0577dafbb4fe2fb6d8f0e8ee02e
RikiMuzzi <riccardo.muzzi02@gmail.com>
2025-03-28 16:29:23 +0100

    calendario impostato

--- server/client/src/components/tomato/tomato.js
+++ server/client/src/components/tomato/tomato.js
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect, useRef } from 'react';
 import './tomato.css';
 
@@ -209,60 +208,71 @@ export default function PomodoroPage() {
   }, [isRunning, timeLeft]);
 
   return (
-    <div className="container">
+    <div className="all">
       <div className="timer-box">
-        <div className="controls">
-          <button 
-            className='button-up'
-            onClick={() => { setTotalMinutes((prev) => Number(prev) - 15); 
-            calculateCustomCycles(Number(totalMinutes) - 15); }}> -
-          </button>
-          <span className="time-display">{totalMinutes} min</span>
-          <button 
-            className='button-up'
-            onClick={() => { setTotalMinutes((prev) => Number(prev) + 15); 
-            calculateCustomCycles(Number(totalMinutes) + 15); }}> +
-          </button>
-        </div>
-
-        <form className="time-info">
-          <div className="time-grid">
-            <div className="info-box">
-              <label> Study </label>
-              <span> {studyDuration} min </span>
+        <div className="up">
+          <div className="controls">
+            <div className="upper-up">
+              <button 
+                className='button-up'
+                onClick={() => { setTotalMinutes((prev) => Number(prev) - 15); 
+                calculateCustomCycles(Number(totalMinutes) - 15); }}> -
+              </button>
+              <button
+                className='info'>
+                  i
+              </button>
             </div>
-            <div className="info-box">
-              <label> Pause </label>
-              <span> {pauseDuration} min </span>
-            </div>
-            <div className="info-box">
-              <label> Cycles </label>
-              <span> {cycles} </span>
-            </div>
-          </div>
-        </form>
-
-        <div className="buttons bottom">
-          <button 
-            className='start'          
-            onClick={handleStartStop}> {isRunning ? 'Stop' : 'Start'} 
-          </button>
-          <div className='hidden-buttons'>
+            
+            <span className="time-display">{totalMinutes} min</span>
             <button 
-              className='cycle-button'
-              onClick={handleRestart}> Restart
-            </button>
-            <button 
-              className='cycle-button'
-              onClick={handleEndCycle}> End Tomato
+              className='button-up'
+              onClick={() => { setTotalMinutes((prev) => Number(prev) + 15); 
+              calculateCustomCycles(Number(totalMinutes) + 15); }}> +
             </button>
           </div>
-        </div>
 
-        <div className="cycle-info"> Cycle {currentCycle}/{cycles} </div>
-        <div className="timer-display">
-          {Math.floor(timeLeft / 60).toString().padStart(2, '0')}:
-          {(timeLeft % 60).toString().padStart(2, '0')}
+          <form className="time-info">
+            <div className="time-grid">
+              <div className="info-box">
+                <label> Study </label>
+                <span> {studyDuration} min </span>
+              </div>
+              <div className="info-box">
+                <label> Pause </label>
+                <span> {pauseDuration} min </span>
+              </div>
+              <div className="info-box">
+                <label> Cycles </label>
+                <span> {cycles} </span>
+              </div>
+            </div>
+          </form>
+        </div>
+        
+        <div className='bot'>
+          <div className="buttons bottom">
+            <button 
+              className='start'          
+              onClick={handleStartStop}> {isRunning ? 'Stop' : 'Start'} 
+            </button>
+            <div className='hidden-buttons'>
+              <button 
+                className='cycle-button'
+                onClick={handleRestart}> Restart
+              </button>
+              <button 
+                className='cycle-button'
+                onClick={handleEndCycle}> End Tomato
+              </button>
+            </div>
+          </div>
+
+          <div className="cycle-info"> Cycle {currentCycle}/{cycles} </div>
+          <div className="timer-display">
+            {Math.floor(timeLeft / 60).toString().padStart(2, '0')}:
+            {(timeLeft % 60).toString().padStart(2, '0')}
+          </div>
         </div>
       </div>
     </div>
